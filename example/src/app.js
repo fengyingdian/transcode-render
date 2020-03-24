9ec789e41118db2b0aa6fe8c785da9df2ab020cc
break <fengyingdian@126.com>
2020-03-24 17:17:25 +0800

    add first published npm package

---
+++ example/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ReactCat from 'react-break-cat';
+
+const App = () => <ReactCat />;
+render(<App />, document.getElementById('root'));
---
+++ node_modules/react-break-cat/README.md
@@ -0,0 +1,2 @@
+# react-cat
+a npm package test
