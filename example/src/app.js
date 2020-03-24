cb311cd381814b95935d5b5558591f70fdafd390
break <fengyingdian@126.com>
2020-03-24 17:32:17 +0800

    remove node_modules

--- example/src/app.js
+++ example/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ReactCat from 'react-break-cat';
+import ReactCat from 'react-break-cat/lib';
+import 'react-break-cat/lib/main.min.css';
 
 const App = () => <ReactCat />;
 render(<App />, document.getElementById('root'));
